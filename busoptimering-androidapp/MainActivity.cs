1a72db828b075c4c3f433214c0d07d8217e89c69
Anantonon <hezarkit@msn.com>
2017-04-06 09:13:18 +0200

    Add project files.

---
+++ busoptimering-androidapp/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace busoptimering_androidapp
+{
+    [Activity(Label = "busoptimering_androidapp", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
